0a7f3c2850bfba27d66909d0def60cdfdc6df6c4
Vasiliy <vasilij.1357@yandex.ru>
2021-04-27 15:31:06 +0300

    Make search bar that can make AJAX requests

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,40 @@
 <template>
   <div id="app">
-    <Preloader v-if="true" />
+    <Preloader v-if="false" />
+    <SearchBar
+      v-else
+      :current-input="currentInput"
+      @change-current-input="changeCurrentInput"
+      @find-weather="findWeather"
+    />
   </div>
 </template>
 
 <script>
 import Preloader from "./components/Preloader"
+import SearchBar from "./components/SearchBar"
 
 export default {
   name: 'App',
   components: {
     Preloader,
+    SearchBar
+  },
+  data() {
+    return {
+      currentInput: 'Moscow',
+      API_KEY: '1d55a69956adecf00c5bebb2a3c89b73'
+    }
+  },
+  methods: {
+    changeCurrentInput(newValue) {
+      this.currentInput = newValue
+    },
+    async findWeather(city) {
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city.toLowerCase()}&appid=${this.API_KEY}&units=metric`)
+      const data = await response.json()
+      console.log(data)
+    }
   }
 }
 </script>
@@ -22,7 +46,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 * {
   box-sizing: border-box;
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="search-bar">
+    <input
+      type="text"
+      class="input"
+      placeholder="Enter city name"
+      :value="currentInput"
+      @input="$emit('change-current-input', $event.target.value)"
+    />
+    <button
+      class="button"
+      @click="$emit('find-weather', currentInput)"
+    >
+      Search
+    </button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'SearchBar',
+    props: {
+      currentInput: String
+    }
+  }
+</script>
+
+<style scoped>
+.search-bar {
+  width: 90%;
+  max-width: 700px;
+  padding-top: 20px;
+  margin: 0 auto 30px;
+}
+.input {
+  width: 70%;
+  min-height: 42px;
+  padding: 15px 16px;
+  font-size: 16px;
+  outline: none;
+  box-shadow: 0px 0px 8px rgba(0,0,0,0.25);
+  color: #313131;
+  background-color: rgba(255,255,255,0.15);
+  border: none;
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+}
+
+.input:hover, .input:focus {
+  box-shadow: 0px 0px 16px rgba(0,0,0,0.25);
+  background-color: rgba(255,255,255,0.75);
+  border-radius: 16px 0px 16px 0px;
+}
+
+.button {
+  min-height: 42px;
+  margin-left: 20px;
+  padding: 15px 16px;
+  font-size: 16px;
+  outline: none;
+  box-shadow: 0px 0px 8px rgba(0,0,0,0.25);
+  border: none;
+  border-radius: 5px;
+  background-color: rgba(86, 180, 255, 0.15);
+  color: #3635ff;
+}
+
+.button:hover {
+  box-shadow: 0px 0px 8px rgba(0,0,0,0.55);
+  cursor: pointer;
+}
+</style>
